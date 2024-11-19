93ff0812231089480f92b26684d5b39d022e19ad
hero <eshimbekovabdurahim@gmail.com>
2024-11-19 14:10:49 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+VANTA.GLOBE({
+      el: "#anim_globe",
+      mouseControls: false,
+      touchControls: false,
+      gyroControls: false,
+      minHeight: 500.00,
+      minWidth: 500.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      size:0.90,
+      color2: 0xF2921B,
+      color: 0x081E26,
+      backgroundColor: 0x05F2DB,
+    })
+  
+  VANTA.NET({
+    el: "#anim_net",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x05F2DB,
+    backgroundColor: 0x081E26,
+    points: 6.00,
+    maxDistance: 23.00,
+    spacing: 20.00
+  })
